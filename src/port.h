f35443291c7f8323701488beeb3602ff09f8b65b
Maxim Kulkin <maxim.kulkin@gmail.com>
2018-04-07 23:29:07 -0700

    Update spi_flash API to support ESP_IDF

--- src/port.h
+++ src/port.h
@@ -4,3 +4,16 @@
 
 uint32_t homekit_random();
 void homekit_random_fill(uint8_t *data, size_t size);
+
+#ifdef ESP_OPEN_RTOS
+#include <spiflash.h>
+#define ESP_OK 0
+#endif
+
+#ifdef ESP_IDF
+#include <esp_spi_flash.h>
+#define SPI_FLASH_SECTOR_SIZE SPI_FLASH_SEC_SIZE
+#define spiflash_read(addr, buffer, size) (spi_flash_read((addr), (buffer), (size)) == ESP_OK)
+#define spiflash_write(addr, data, size) (spi_flash_write((addr), (data), (size)) == ESP_OK)
+#define spiflash_erase_sector(addr) (spi_flash_erase_sector((addr) / SPI_FLASH_SECTOR_SIZE) == ESP_OK)
+#endif
